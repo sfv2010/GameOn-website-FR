9832e752b5c7768fa97688f6ae84caa5f275aece
Sayaka Vincent <sfv2010@hotmail.fr>
2023-01-23 17:43:01 +0100

    Cclose the modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,6 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+closeBtn.addEventListener("click", () => {
+  modalbg.style.display = "none";
+});
