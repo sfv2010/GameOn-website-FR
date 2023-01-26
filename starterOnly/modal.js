f138bc0438252f0557082abbd73a5402ddf426cc
Sayaka Vincent <sfv2010@hotmail.fr>
2023-01-26 22:48:04 +0100

    fix validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -26,109 +26,100 @@ function launchModal() {
 closeBtn.addEventListener("click", () => {
   modalbg.style.display = "none";
 });
-// const closeModalBtn = () => {
-//   modalbg.style.display = "none";
-// };
-// closeBtn.addEventListener("click", closeModalBtn);
 
 //-------Ajouter validation ou messages d'erreur
-const firstElem = document.getElementById("first");
 
 function validate() {
-  if (firstElem.value === "") {
-    alert("Merci de remplir tous les champs.");
-    return false;
-  } else {
-    return true;
-  }
-}
+  const validationForm = document.querySelector(".validationForm");
+  if (validationForm) {
+    const textControlElems = document.querySelectorAll(".text-control");
+    const firstElem = document.getElementById("first");
+    const lastElem = document.getElementById("last");
+    const emailElem = document.getElementById("email");
+    const birthdateElem = document.getElementById("birthdate");
+    const quantityElem = document.getElementById("quantity");
+    // const selectElem = document.getElementById("select");
 
-const validationForm = document.querySelector(".validationForm");
-if (validationForm) {
-  const errorClassName = "error";
+    // console.log(selectElem);
 
-  const textControlElems = document.querySelectorAll(".text-control");
-  const firstElem = document.getElementById("first");
-  const lastElem = document.getElementById("last");
-  const emailElem = document.getElementById("email");
-  const birthdateElem = document.getElementById("birthdate");
-  const quantityElem = document.getElementById("quantity");
-  const selectElem = document.getElementById("select");
+    const createError = (elem, errorMessage) => {
+      //span element
+      const errorSpan = document.createElement("span");
+      //ajouter class pour error
+      errorSpan.classList.add("error");
+      //aria-live 属性を設定
+      errorSpan.setAttribute("data-error-visible", "true");
+      //message error
+      errorSpan.textContent = errorMessage;
+      //add elem
+      elem.parentNode.appendChild(errorSpan);
 
-  console.log(selectElem);
+      // textControlElems.forEach((elem) => {
+      //   elem.classList.add("inputError");
+      // });
+    };
 
-  const createError = (elem, errorMessage) => {
-    //span element
-    const errorSpan = document.createElement("span");
-    //ajouter class pour error
-    errorSpan.classList.add(errorClassName);
-    //message error
-    errorSpan.textContent = errorMessage;
-    //add elem
-    elem.parentNode.appendChild(errorSpan);
-  };
+    validationForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      //Récupérer tous les éléments qui affichent des erreurs et les supprimer
+      const errorElems = validationForm.querySelectorAll(".error ");
+      errorElems.forEach((elem) => {
+        elem.remove();
+      });
 
-  validationForm.addEventListener("submit", (e) => {
-    //
-    const errorElems = validationForm.querySelectorAll("." + errorClassName);
-    errorElems.forEach((elem) => {
-      elem.remove();
-    });
+      const firstValue = firstElem.value.trim();
+      if (firstElem.value === "" || firstValue.length <= 2) {
+        createError(
+          firstElem,
+          "Veuillez entrer 2 caractères ou plus pour le champ du Prénom."
+        );
+        firstElem.classList.add("inputError");
+      } else {
+        firstElem.classList.remove("inputError");
+      }
 
-    //Validation de .textControlElems
-    // textControlElems.forEach((elem) => {
-    //   //Supprimer les espaces avant et après la valeur
-    //   const elemValue = elem.value.trim();
-    //   //Afficher l'erreur et abandonner la soumission du formulaire
-    //   if (elemValue.length === 0) {
-    //     createError(elem, "Merci de remplir tous les champs.");
-    //     e.preventDefault();
-    //   }
-    // });
-
-    if (firstElem.value === "" || firstElem.value.length <= 2) {
-      createError(
-        firstElem,
-        "Veuillez entrer 2 caractères ou plus pour le champ du Prénom."
-      );
-    }
-    if (lastElem.value === "" || lastElem.value.length <= 2) {
-      createError(
-        lastElem,
-        "Veuillez entrer 2 caractères ou plus pour le champ du Nom."
-      );
-    }
-    //Validation email
-    const pattern =
-      /^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/iu;
-    //test() affichez une erreur et arrêtez de soumettre le formulaire
-    if (emailElem.value === "" || !pattern.test(emailElem.value)) {
-      createError(emailElem, "Veuillez entrer une adresse mail valide.");
+      const lastValue = lastElem.value.trim();
+      if (lastElem.value === "" || lastValue.length <= 2) {
+        createError(
+          lastElem,
+          "Veuillez entrer 2 caractères ou plus pour le champ du Nom."
+        );
+        lastElem.classList.add("inputError");
+      } else {
+        lastElem.classList.remove("inputError");
+      }
+      //Validation email
+      const pattern =
+        /^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/iu;
+      //test() affichez une erreur et arrêtez de soumettre le formulaire
+      if (emailElem.value === "" || !pattern.test(emailElem.value)) {
+        createError(emailElem, "Veuillez entrer une adresse mail valide.");
+        emailElem.classList.add("inputError");
+      } else {
+        emailElem.classList.remove("inputError");
+      }
 
       //Validation date de naissance
       if (birthdateElem.value === "") {
         createError(birthdateElem, "Veuillez entrer une date de naissance.");
+        birthdateElem.classList.add("inputError");
+      } else {
+        birthdateElem.classList.remove("inputError");
       }
 
       if (quantityElem.value === "") {
         createError(quantityElem, "Veuillez entrer un chiffre.");
+        quantityElem.classList.add("inputError");
+      } else {
+        quantityElem.classList.remove("inputError");
       }
 
       // if (selectElem.value === "") {
       //   createError(selectElem, "Veuillez sélectionner un choix.");
       // }
-
-      e.preventDefault();
-    }
-  });
+    });
+  }
 }
 
-// submit.addEventListener(
-//   "click",
-//   (e) => {
-//     e.preventDefault();
-//     const first = document.getElementsByName("first");
-//     console.log(first[0].value);
-//   },
-//   false
-// );
+const validForm = document.querySelector(".validationForm");
+validForm.addEventListener("click", validate);
