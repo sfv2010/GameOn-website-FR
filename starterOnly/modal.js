f484c6808fb8c6ee4922f9ab3c0e7607b530b196
Sayaka Vincent <sfv2010@hotmail.fr>
2023-02-02 14:38:40 +0100

    remove unnecessary addEventListener

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -26,21 +26,17 @@ const validationForm = document.querySelector(".validationForm");
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal(e) {
     modalbg.style.display = "block";
-    //modalConfirmation.style.display = "none";
 }
 
 //----fermer la modale avec close
 closeBtn.addEventListener("click", () => {
     modalbg.style.display = "none";
-    // if (modalConfirmation.style.display === "block") {
-    //     modalConfirmation.style.display = "none";
-    //     modalbody.style.display = "block";
-    //     validationForm.reset();
-    // }
 });
 
 //-------Function validation
-function validate() {
+
+function validate(e) {
+    e.preventDefault();
     //fonction pour afficher les  messages d'erreurs
     const createError = (elem, errorMessage) => {
         //créer span element
@@ -53,147 +49,127 @@ function validate() {
         elem.parentNode.appendChild(errorSpan);
     };
 
-    //Event soumission
-    validationForm.addEventListener("submit", (e) => {
-        e.stopImmediatePropagation(); //sinon modalConfirmation va s'afficher plusieurs
+    // Récupérer tous les éléments qui affichent des erreurs et les supprimer après chaque soumission
+    const errorElems = validationForm.querySelectorAll(".error ");
+    errorElems.forEach((elem) => {
+        elem.remove();
+    });
 
-        //Récupérer tous les éléments qui affichent des erreurs et les supprimer après chaque soumission
-        const errorElems = validationForm.querySelectorAll(".error ");
-        errorElems.forEach((elem) => {
-            elem.remove();
-        });
+    //---------Validation name---------
+    const firstValue = firstElem.value.trim();
+    if (firstValue.length <= 2) {
+        createError(
+            firstElem,
+            "Veuillez entrer 2 caractères ou plus pour le champ du Prénom."
+        );
+        firstElem.classList.add("inputError");
+    } else {
+        firstElem.classList.remove("inputError");
+    }
 
-        //---------Validation name---------
-        const firstValue = firstElem.value.trim();
-        if (firstValue.length <= 2) {
-            createError(
-                firstElem,
-                "Veuillez entrer 2 caractères ou plus pour le champ du Prénom."
+    const lastValue = lastElem.value.trim();
+    if (lastValue.length <= 2) {
+        createError(
+            lastElem,
+            "Veuillez entrer 2 caractères ou plus pour le champ du Nom."
+        );
+        lastElem.classList.add("inputError");
+    } else {
+        lastElem.classList.remove("inputError");
+    }
+    //---------Validation email---------
+    const pattern =
+        /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]+.[A-Za-z0-9]+$/;
+    //test() affichez une erreur et arrêtez de soumettre le formulaire
+    if (emailElem.value === "" || !pattern.test(emailElem.value)) {
+        createError(emailElem, "Veuillez entrer une adresse mail valide.");
+        emailElem.classList.add("inputError");
+    } else {
+        emailElem.classList.remove("inputError");
+    }
+
+    //---------Validation date de naissance---------
+    if (birthdateElem.value === "") {
+        createError(birthdateElem, "Veuillez entrer une date de naissance.");
+        birthdateElem.classList.add("inputError");
+    } else {
+        birthdateElem.classList.remove("inputError");
+    }
+    const patternNumber = /^[0-9]+$/;
+    if (quantityElem.value === "" || !patternNumber.test(quantityElem.value)) {
+        createError(quantityElem, "Veuillez entrer un chiffre.");
+        quantityElem.classList.add("inputError");
+    } else {
+        quantityElem.classList.remove("inputError");
+    }
+
+    //---------Validation checkBox-----------
+    checkBoxElem.forEach((elem) => {
+        if (elem.getAttribute("type") === "radio") {
+            // Récupère le premier élément de case à cocher sélectionné en fonction de l'élément parent
+            const checked = elem.parentElement.querySelector(
+                'input[type="radio"]:checked'
             );
-            firstElem.classList.add("inputError");
-            e.preventDefault();
-        } else {
-            firstElem.classList.remove("inputError");
-        }
-
-        const lastValue = lastElem.value.trim();
-        if (lastValue.length <= 2) {
-            createError(
-                lastElem,
-                "Veuillez entrer 2 caractères ou plus pour le champ du Nom."
+            //console.log(elem.parentElement);
+            if (checked === null) {
+                createError(elem, "Veuillez sélectionner un choix.");
+            }
+        } else if (elem.getAttribute("id") === "checkbox1") {
+            const checkedBox2 = elem.parentElement.querySelector(
+                'input[id="checkbox1"]:checked'
             );
-            lastElem.classList.add("inputError");
-            e.preventDefault();
-        } else {
-            lastElem.classList.remove("inputError");
-        }
-        //---------Validation email---------
-        const pattern =
-            /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]+.[A-Za-z0-9]+$/;
-        //test() affichez une erreur et arrêtez de soumettre le formulaire
-        if (emailElem.value === "" || !pattern.test(emailElem.value)) {
-            createError(emailElem, "Veuillez entrer une adresse mail valide.");
-            emailElem.classList.add("inputError");
-            e.preventDefault();
-        } else {
-            emailElem.classList.remove("inputError");
-        }
-
-        //---------Validation date de naissance---------
-        if (birthdateElem.value === "") {
-            createError(
-                birthdateElem,
-                "Veuillez entrer une date de naissance."
-            );
-            birthdateElem.classList.add("inputError");
-            e.preventDefault();
-        } else {
-            birthdateElem.classList.remove("inputError");
-        }
-        const patternNumber = /^[0-9]+$/;
-        if (
-            quantityElem.value === "" ||
-            !patternNumber.test(quantityElem.value)
-        ) {
-            createError(quantityElem, "Veuillez entrer un chiffre.");
-            quantityElem.classList.add("inputError");
-            e.preventDefault();
-        } else {
-            quantityElem.classList.remove("inputError");
-        }
-
-        //---------Validation checkBox-----------
-        checkBoxElem.forEach((elem) => {
-            if (elem.getAttribute("type") === "radio") {
-                // Récupère le premier élément de case à cocher sélectionné en fonction de l'élément parent
-                const checked = elem.parentElement.querySelector(
-                    'input[type="radio"]:checked'
+            if (checkedBox2 === null) {
+                createError(
+                    elem,
+                    "Vous devez vérifier que vous acceptez les termes et conditions."
                 );
-                //console.log(elem.parentElement);
-                if (checked === null) {
-                    createError(elem, "Veuillez sélectionner un choix.");
-                }
-                e.preventDefault();
-            } else if (elem.getAttribute("id") === "checkbox1") {
-                const checkedBox2 = elem.parentElement.querySelector(
-                    'input[id="checkbox1"]:checked'
-                );
-                if (checkedBox2 === null) {
-                    createError(
-                        elem,
-                        "Vous devez vérifier que vous acceptez les termes et conditions."
-                    );
-                }
-                e.preventDefault();
+            }
+        }
+    });
+
+    //---------modal confirmation-----------
+    //créer un modal de confirmation
+    const modalContent = document.querySelector(".content");
+    const modalConfirmation = document.createElement("div");
+    modalConfirmation.classList.add("confirmation");
+    const confirmationP = document.createElement("p");
+    confirmationP.classList.add("confirmationP");
+    confirmationP.innerHTML = "Merci pour <br> votre inscription";
+    const confirmationButton = document.createElement("button");
+    confirmationButton.classList.add(
+        "confirmationButton",
+        "button",
+        "btn-submit"
+    );
+    confirmationButton.textContent = "Fermer";
+    modalContent.appendChild(modalConfirmation);
+    modalConfirmation.appendChild(confirmationP);
+    modalConfirmation.appendChild(confirmationButton);
+    //Vérifier si tous les formulaires sont remplis sans erreur
+    const isRequired = validationForm.checkValidity();
+    if (isRequired === false) {
+        modalbody.style.display = "block";
+        modalConfirmation.style.display = "none";
+    } else {
+        modalbody.style.display = "none";
+        modalConfirmation.style.display = "block";
+        //Event pour fermer modal confirmation avec closebtn
+        closeBtn.addEventListener("click", () => {
+            if (modalConfirmation.style.display === "block") {
+                modalConfirmation.style.display = "none";
+                modalbody.style.display = "block";
+                validationForm.reset();
             }
         });
+    }
 
-        //---------modal confirmation-----------
-        //créer un modal de confirmation
-        const modalContent = document.querySelector(".content");
-        const modalConfirmation = document.createElement("div");
-        modalConfirmation.classList.add("confirmation");
-        const confirmationP = document.createElement("p");
-        confirmationP.classList.add("confirmationP");
-        confirmationP.innerHTML = "Merci pour <br> votre inscription";
-        const confirmationButton = document.createElement("button");
-        confirmationButton.classList.add(
-            "confirmationButton",
-            "button",
-            "btn-submit"
-        );
-        confirmationButton.textContent = "Fermer";
-        modalContent.appendChild(modalConfirmation);
-        modalConfirmation.appendChild(confirmationP);
-        modalConfirmation.appendChild(confirmationButton);
-        //Vérifier si tous les formulaires sont remplis sans erreur
-        const isRequired = validationForm.checkValidity();
-        if (isRequired === false) {
-            modalbody.style.display = "block";
-            modalConfirmation.style.display = "none";
-            e.preventDefault();
-        } else {
-            modalbody.style.display = "none";
-            modalConfirmation.style.display = "block";
-            //Event pour fermer modal confirmation avec closebtn
-            closeBtn.addEventListener("click", () => {
-                if (modalConfirmation.style.display === "block") {
-                    modalConfirmation.style.display = "none";
-                    modalbody.style.display = "block";
-                    validationForm.reset();
-                }
-            });
-        }
-
-        //Event pour fermer modal confirmation
-        confirmationButton.addEventListener("click", () => {
-            modalbg.style.display = "none";
-            modalConfirmation.style.display = "none";
-            modalbody.style.display = "block";
-            validationForm.reset();
-        });
+    //Event pour fermer modal confirmation
+    confirmationButton.addEventListener("click", () => {
+        modalbg.style.display = "none";
+        modalConfirmation.style.display = "none";
+        modalbody.style.display = "block";
+        validationForm.reset();
     });
-    // return false;
 }
 
-submit.addEventListener("click", validate);
+validationForm.addEventListener("submit", validate);
